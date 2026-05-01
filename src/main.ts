91558c8e19b0610190a7b3b853a7a0b7d36bbfa2
{Calvin Iordye <iordyecaln@gmail.com>
2026-05-01 12:28:57 +0100

    fix(main): update CORS fallback to new frontend URL

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
 app.enableCors({
   origin: [
-    process.env.WEB_PORTAL_URL || 'https://insighta-web-production-312d.up.railway.app',
+    process.env.WEB_PORTAL_URL || 'https://insighta-web-production-df68.up.railway.app',
     'http://localhost:3001',
   ],
   credentials: true,
